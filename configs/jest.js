b8518dc330d6b3e2c977bf35dfc1c2d6a97fd525
Blake Embrey <hello@blakeembrey.com>
2021-04-09 13:26:07 -0700

    Support older node versions

--- configs/jest.js
+++ configs/jest.js
@@ -1,14 +1,14 @@
 const { posix } = require("path");
 
 /** @type {import("../src/common").Config} */
-const config = JSON.parse(process.env.TS_SCRIPTS_CONFIG ?? "{}");
+const config = JSON.parse(process.env.TS_SCRIPTS_CONFIG || "{}");
 
 module.exports = {
   projects: config.test.map((test) => {
     /** @type {import("ts-jest/dist/types").InitialOptionsTsJest} */
     const options = {
       rootDir: config.dir,
-      roots: (test.dir ?? config.src).map((x) => posix.join("<rootDir>", x)),
+      roots: (test.dir || config.src).map((x) => posix.join("<rootDir>", x)),
       testEnvironment: test.env,
       globals: {
         "ts-jest": {
