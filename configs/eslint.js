166637bca61ebe3aa12cb199ac12cbba9221c57a
Blake Embrey <hello@blakeembrey.com>
2021-10-21 22:30:50 -0700

    Use correct TS ESLint recommended config

--- configs/eslint.js
+++ configs/eslint.js
@@ -2,7 +2,7 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
     "prettier",
   ],
   parserOptions: {
