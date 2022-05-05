7e2d3bac92285f153110f91399a3a3a717e89ac5
Blake Embrey <hello@blakeembrey.com>
2022-05-05 16:57:19 -0700

    Pin lint staged

--- configs/lint-staged.cjs
+++ configs/lint-staged.cjs
@@ -1,9 +1,4 @@
-const { join, basename } = require("path");
-
-const script =
-  basename(join(__dirname, "..", "..")) === "node_modules"
-    ? "ts-scripts"
-    : `node '${require.resolve("../dist/bin.js")}'`;
+const script = `node '${require.resolve("../dist/bin.js")}'`;
 
 module.exports = {
   [process.env.TS_SCRIPTS_LINT_GLOB]: `${script} lint --filter-paths`,
